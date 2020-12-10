3f713350d4cca7ef79daca2e2c3d5eed2e4e915e
armaank <kohli@cooper.edu>
2020-12-10 12:23:12 -0500

    Update requirements, add nltk download

---
+++ nltk_download.py
@@ -0,0 +1,10 @@
+"""
+downloads all required nltk packages
+"""
+
+import nltk
+
+nltk.download("stopwords")  # set of stopwords
+nltk.download("punkt")  # sentence tokenizer
+nltk.download("wordnet")  # wordnet corpus
+nltk.download("averaged_perceptron_tagger")  # pos tagger
