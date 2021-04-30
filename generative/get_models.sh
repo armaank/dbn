a39970fb20c0cb591d1d9a55492f94f2088fdb7f
armaank <kohli@cooper.edu>
2021-04-30 18:53:08 -0400

    adding model downloading script

---
+++ generative/get_models.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+
+mkdir models
+cd models
+mkdir adaily-1024
+cd adaily-1024
+mkdir torch_official
+cd torch_official
+gdown --id 1APtwpZj7uyzY4mRYMz9uoB2IgcXknlqB
+cd ..
+mkdir torch_custom
+mkdir tf_official
+cd tf_official
+gdown --id 1qYWtbv9YvX3SGIVpW3493uY78hdiwJAh
+cd ..
+cd torch_custom
+gdown --id 10bpWfzTdnx3f65sIYeIJVUn-2zAOlTiv
+gdown --id 1tE86w0zcOtnP6Th683u6Aj0dbEruU3_V
+gdown --id 1Qe-sqg_UQq1bXX8zLvvPkbavAt3gfynA
+cd
